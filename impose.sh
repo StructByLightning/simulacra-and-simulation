15341f69208126cf610b1bb44ec57fff93bda412
StructByLightning <mossygreenrock@gmail.com>
2025-07-18 07:41:33 -0700

    first

---
+++ impose.sh
@@ -0,0 +1,7 @@
+pdfbook2 --paper=legalpaper --signature=16 \
+  --outer-margin=0 \
+  --inner-margin=0 \
+  --top-margin=0 \
+  --bottom-margin=0 \
+  --no-crop \
+  output.pdf
---
+++ src/index.js
@@ -0,0 +1,5 @@
+// Import our styles
+import './styles/main.scss';
+
+// Any JavaScript code can go here
+console.log('Webpack is working!');
